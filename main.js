1f9c93f1d190888261b335db0d30d6d0cb772efb
Eric Henrique Alves Vicente <106555851+VicenteEric@users.noreply.github.com>
2024-02-27 14:30:30 -0300

    o que vai adianaatr

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
@@ -66,13 +66,12 @@ document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
 document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
 
 function atualizaCronometro(){
-    for (let i=0; i<contadores.length;i++){
-        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
-        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
-        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
-        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
-        console.log("seg"+i);
-    }
+} for (let i=0; i<contadores.length;i++){
+    document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+    document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+    document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+    document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
 }
+
 
 
