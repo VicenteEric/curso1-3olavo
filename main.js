7f7d66d3878a70f1f248dd47d30876798f518eaa
Eric Henrique Alves Vicente <106555851+VicenteEric@users.noreply.github.com>
2024-03-13 10:45:19 -0300

    meudeusinho

--- main.js
+++ main.js
@@ -34,24 +34,19 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-
-     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-
     if (tempoFinal > 0){
+        return [dias,horas,minutos,segundos];
     } else {
         return [0,0,0,0];
-        return [dias,horas,minutos,segundos];
     }
 }
 
 function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
-    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
-    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
-
     for (let i=0; i<contadores.length;i++){
-       // contadores[i].textContent = calculaTempo(tempos[i]);   
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];   
     }
 }
 
@@ -61,21 +56,3 @@ function comecaCronometro(){
 }
 
 comecaCronometro();
-
-
-document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
-document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
-document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
-document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
-
-function atualizaCronometro(){
-    
-} for (let i=0; i<contadores.length;i++){
-    document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
-    document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
-    document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
-    document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
-}
-
-
-
